f9bfaa248c9933a7fa60c976d47c2026038b8656
cartscode <cartercarig@gmail.com>
2025-10-27 23:28:29 +0800

    database connection in Sign up. need a little improvement in Ui like in hamburger nav.

---
+++ signup/signup.php
@@ -0,0 +1,25 @@
+<?php
+include('config.php');
+
+if (isset($_POST['signup'])) {
+    $name = $_POST['name'];
+    $number = $_POST['number'];
+    $email = $_POST['email'];
+    $emergency = $_POST['emergency'];
+    $username = $_POST['username'];
+    $password = password_hash($_POST['password'], PASSWORD_DEFAULT);
+    $month = $_POST['month'];
+    $day = $_POST['day'];
+    $year = $_POST['year'];
+    $gender = $_POST['gender'];
+
+    $sql = "INSERT INTO users (name, number, email, emergency_contact, username, password, birth_month, birth_day, birth_year, gender)
+            VALUES ('$name', '$number', '$email', '$emergency', '$username', '$password', '$month', '$day', '$year', '$gender')";
+
+    if (mysqli_query($conn, $sql)) {
+        echo "<script>alert('Signup successful!'); window.location.href='login.html';</script>";
+    } else {
+        echo "Error: " . mysqli_error($conn);
+    }
+}
+?>

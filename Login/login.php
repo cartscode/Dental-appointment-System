14c7f4d1263276df492b51dee2f51e9585504e19
cartscode <cartercarig@gmail.com>
2025-10-28 11:16:17 +0800

    Login & Sign up is working

---
+++ Login/login.php
@@ -0,0 +1,32 @@
+<?php
+include('config.php');
+session_start();
+
+if (isset($_POST['login'])) {
+    $username = $_POST['username'];
+    $password = $_POST['password'];
+
+    // Fetch user from DB
+    $sql = "SELECT * FROM users WHERE username = '$username'";
+    $result = $conn->query($sql);
+
+    if ($result->num_rows > 0) {
+        $user = $result->fetch_assoc();
+
+        // Verify password
+        if (password_verify($password, $user['password'])) {
+            $_SESSION['username'] = $user['username'];
+            echo "<script>
+                    alert('Login successful!');
+                    window.location.href='http://localhost/Project%20in%20IS104/Appointment%20Page/AppointmentsBooking.html';
+                  </script>";
+        } else {
+            echo "<script>alert('Incorrect password! Please try again.'); window.history.back();</script>";
+        }
+    } else {
+        echo "<script>alert('Username not found!'); window.history.back();</script>";
+    }
+
+    $conn->close();
+}
+?>

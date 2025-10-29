8e79cc8357e6653a51c9d1823f7c3296ede4d5f7
Kenpnll <kenpnll@gmail.com>
2025-10-30 00:35:55 +0800

    Add files via upload

---
+++ About Us/script.js
@@ -0,0 +1,9 @@
+  const toggle = document.getElementById('menu-toggle');
+  const navLinks = document.getElementById('nav-links');
+  const navRight = document.querySelector('.nav-right');
+
+  toggle.addEventListener('click', () => {
+    navLinks.classList.toggle('active');
+    navRight.classList.toggle('active');
+  });
+

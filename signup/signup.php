976c0a8ff768675648a3d37f15abfabdd92defa6
cartscode <cartercarig@gmail.com>
2025-11-27 01:01:46 +0800

    updating the new column in database which is last name and first name and term_accepted. removing the name. debugging the signup.php. Now is working! -Carter

--- signup/signup.php
+++ signup/signup.php
@@ -10,18 +10,18 @@ if (isset($_POST['signup'])) {
     $username = $_POST['username']; // <-- added
     $email = $_POST['email']; 
     $password = password_hash($_POST['password'], PASSWORD_DEFAULT); 
-    $month = $_POST['month'];
-    $day = $_POST['day'];
-    $year = $_POST['year'];
+    $birth_month = $_POST['month'];
+    $birth_day = $_POST['day'];
+    $birth_year = $_POST['year'];
     $gender = $_POST['gender'];          
     $terms_accepted = isset($_POST['terms']) ? 1 : 0; // checkbox
 
     // Insert data into database
-    $sql = "INSERT INTO users (first_name, last_name, username, month, day, year, gender, email, password, terms_accepted)
-            VALUES ('$first_name', '$last_name', '$username', '$month', '$day', '$year', '$gender', '$email', '$password', $terms_accepted)";
+    $sql = "INSERT INTO users (first_name, last_name, username, birth_month, birth_day, birth_year, gender, email, password, terms_accepted)
+            VALUES ('$first_name', '$last_name', '$username', '$birth_month', '$birth_day', '$birth_year', '$gender', '$email', '$password', $terms_accepted)";
 
     if (mysqli_query($conn, $sql)) {
-        echo "<script>alert('Signup successful!'); window.location.href='login.html';</script>";
+        echo "<script>alert('Signup successful!'); window.location.href='/Project in IS104/Login/login.html';</script>";
     } else {
         echo "Error: " . mysqli_error($conn);
     }
